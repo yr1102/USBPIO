5692ac13642acd67fe98ba86c9908df60867e542
yr1102 <41936283+yr1102@users.noreply.github.com>
2018-08-23 17:13:39 +0900

    Add files via upload

--- App/Form2.Designer.vb
+++ App/Form2.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form2
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
@@ -34,11 +35,21 @@ Partial Class Form2
         Me.Button1.Text = "カウンターのリセット"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(178, 52)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(128, 46)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "通信エラーのリセット"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(753, 162)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form2"
         Me.Text = "条件設定"
@@ -47,4 +58,5 @@ Partial Class Form2
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
